13a44cb5973e6329d4de036841537c4fc0b3683b
Ivan Savelo <ivaan2002@mail.ru>
2018-08-10 00:21:24 +0300

    WIP japyrus

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//TODO: Class variable list
+//TODO: operation with variables (count = count + 1)
+//TODO: if else
+//TODO: body of event, function can contain not only call of functions
 
 let japyrus = {};
 
@@ -81,16 +85,200 @@ japyrus.rep = function(pattern, separator) {
     });
 }
 
-japyrus.parseScriptname = function(str) {
-  let findWord = japyrus.rgx(/\w+/i);
-  let findSpace = japyrus.rgx(/\s*/i);
-  let result = japyrus.seq(japyrus.txt('Scriptname'), findSpace, findWord, findSpace, japyrus.txt('extends'), findSpace, findWord);
-  let res1 = result.exec(str, 0);
-  let assertResult = {};
-  assertResult.scriptname = res1.res[2];
-  assertResult.extends = res1.res[4];
-  return assertResult;
+//end of copy
+japyrus.findWord = japyrus.rgx(/\w+/i);
+japyrus.findSpace = japyrus.rgx(/\s+/i); //space must be, if there is no space -> undefined
+japyrus.findOptionalSpace = japyrus.rgx(/\s*/i);
+japyrus.findNumber = japyrus.rgx(/\d+/i);
+japyrus.findVarName = japyrus.rgx(/[\d\w_]+/i);
+japyrus.findVarArg = japyrus.rgx(/[\d\w,".!@#№;$%^:?&*()-_+=~]+/i);
+
+/*
+class variable {
+  var variableList = [];
+
+  getVariable(name) {
+     for(let i = 0; i<this.variableList.length; i++)
+     {
+        if(this.variableList[i].name == name)
+        {
+           return this.variableList[i];
+        }
+     }
+  }
+  push(variable) {
+    this.variableList.push(variable);
+  }
+
 }
 
+japyrus.vars = new variable();
+*/
+
+japyrus.varlist = [];
+japyrus.varlist.getVariable = function(name){
+    for(let i = 0; i<japyrus.varlist.length; i++)
+    {
+       if(japyrus.varlist[i].name == name)
+       {
+         return japyrus.varlist[i];
+       }
+    }
+    return undefined;
+}
+//japyrus.varlist.push = function(variable){
+  //  japyrus.varlist.push(variable);
+//}
+
+japyrus.isString = function(str) {
+    if(str[0] == `"` && str[str.length-1] == `"`) return true;
+    return false;
+}
+
+//japyrus.isInteger = Number.isInteger;
+japyrus.isInt = function(n){
+    return Number(n) === n && n % 1 === 0;
+}
+japyrus.isFloat = function(n){
+    return Number(n) === n && n % 1 !== 0;
+}
+
+japyrus.isVarValueCorrect = function(type, value) {
+  console.log(type + ' '+ typeof(value));
+    if(type.toLowerCase() == "int" && japyrus.isInt(Number(value))) return true;
+    if(type.toLowerCase() == "float" && japyrus.isFloat(parseFloat(value))) return true;
+    if(type.toLowerCase() == "string" && japyrus.isString(value)) return true;
+    if(type.toLowerCase() == "bool" && typeof(value) == typeof(true)) return true;
+    return false;
+}
+
+
+japyrus.parseScriptname = function(str) {
+    let result = japyrus.seq(japyrus.txt('Scriptname'), japyrus.findSpace, japyrus.findWord, japyrus.findSpace, japyrus.txt('extends'), japyrus.findSpace, japyrus.findWord);
+    let res1 = result.exec(str, 0);
+    if(res1 == undefined) return undefined;
+    let assertResult = {};
+    assertResult.scriptname = res1.res[2];
+    assertResult.extends = res1.res[4];
+    return assertResult;
+}
+
+japyrus.parseArguments = function(str) {
+  let args = [];
+  let reg = japyrus.seq(japyrus.findOptionalSpace, japyrus.findWord, japyrus.findSpace, japyrus.findWord);
+  let res;
+  if(str.indexOf(',')!=-1)
+  {
+      let list = str.split(',');
+      for(let i=0; i<list.length;i++)
+      {
+         res = reg.exec(list[i], 0);
+         if(res == undefined) return undefined;
+         args[i] = {};
+         args[i].type = res.res[1];
+         args[i].name = res.res[3];
+      }
+  } else {
+      res = reg.exec(str, 0);
+      if(res == undefined) return undefined;
+      args[0] = {};
+      args[0].type = res.res[1];
+      args[0].name = res.res[3];
+  }
+  return args;
+}
+
+japyrus.parseCertainArguments = function(str) {
+  let args = [];
+  //let reg = japyrus.seq(japyrus.findOptionalSpace, japyrus.findWord, japyrus.findSpace, japyrus.findWord);
+  let trimStr = str.trim();
+  let pos = 0;
+  let isStr = false;
+  for(let i=0; i<trimStr.length; i++)
+  {
+      if(!isStr) {
+          if(trimStr[i]==',') {
+              args.push(trimStr.slice(pos,i).trim());
+              pos = i+1;
+          }
+      }
+      if(trimStr[i]=='"')
+      {
+          isStr = !isStr;
+      }
+  }
+  args.push(trimStr.slice(pos,trimStr.length).trim());
+  return args;
+}
+
+
+
+japyrus.parseCallOfFunction = function(str) {
+    let result = {};
+    let reg = japyrus.seq(japyrus.findOptionalSpace, japyrus.findWord,japyrus.txt('.'), japyrus.findWord);
+    let res = reg.exec(str,0);
+    if(res == undefined) return undefined;
+    result.object = res.res[1];
+    result.method = res.res[3];
+    result.args = japyrus.parseCertainArguments(str.slice(res.end+1,str.length-1));
+    return result;
+}
+/*
+Event OnActivate(ObjectReference akActionRef)
+   Debug.MessageBox("Hello, World!")
+endEvent
+*/
+
+japyrus.parseEvent = function(str) {
+  //let _arguments = //japyrus.txt('Debug.MessageBox("Hello, World!")'), findSpace,
+  let eventResult = {};
+  let beforeArgs = japyrus.seq(japyrus.txt('Event'), japyrus.findSpace, japyrus.findWord);//, japyrus.txt('('), japyrus.findWord, japyrus.findSpace, japyrus.findWord, japyrus.txt(')'), japyrus.findSpace, /*parse arguments*/japyrus.txt('Debug.MessageBox("Hello, World!")'), japyrus.findSpace,/*parse arguments end*/japyrus.txt('endEvent') );
+  let res1 = beforeArgs.exec(str, 0);
+  if(res1 == undefined) return undefined;
+  let pos1 = res1.end+1;
+  let argend1 = str.indexOf(')');
+  let endEvent = str.indexOf('endEvent');
+  if(argend1 == -1 || endEvent == -1) return undefined;
+
+  let args1 = japyrus.parseArguments(str.slice(pos1,argend1));
+  eventResult.blocktype = "Event";
+  eventResult.name = res1.res[2];
+  eventResult.args = args1;
+
+
+
+  eventResult.body = [];
+  let body = str.slice(argend1+1,endEvent).trim().split('\n');
+  console.log("body");
+  console.log(body);
+  for(let i = 0; i<body.length; i++)
+  {
+  //  console.log(japyrus.parseCallOfFunction(body[i]));
+    eventResult.body.push(japyrus.parseCallOfFunction(body[i]));
+  }
+  //eventResult.body = japyrus.parseCallOfFunction(body);
+  return eventResult;
+}
+
+japyrus.parseVar = function(str) {
+  let variable = {};
+  let trimStr = str.trim();
+  let reg = japyrus.seq(japyrus.findVarName, japyrus.findSpace, japyrus.findVarName, japyrus.findOptionalSpace, japyrus.opt(japyrus.seq(japyrus.txt('='), japyrus.findOptionalSpace, japyrus.findVarArg)));
+  let res = reg.exec(str,0);
+  if(res == undefined) return undefined;
+  variable.type = res.res[0].toLowerCase();
+  variable.name = res.res[2];
+    if(res.res[4] == undefined)
+      variable.value = undefined;
+    else
+    {
+      if(!japyrus.isVarValueCorrect(variable.type, res.res[4][2])) return undefined;
+      variable.value = res.res[4][2];
+    }
+  console.log(variable);
+  //japyrus.varlist.push(variable);
+  return variable;
+   //or error: value doesn't match type
+}
 
 module.exports = japyrus;
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+//TODO: delete console.log()
+//TODO: tests with wrong data
+
 const Mocha = require('Mocha');
 const mocha = new Mocha();
 const assert = require('chai').assert;
@@ -6,7 +9,11 @@ const japyrus = require('.');
 
 let ScriptName =
 `Scriptname AAAMPChest        extends              ObjectReference`;
-
+let _Event =
+`Event OnActivate(ObjectReference akActionRef)
+   Debug.MessageBox("Hello, World!", true)
+endEvent`;
+let call = `Debug.MessageBox("Hello, World!")`;
 mocha.run(() => {
      describe('Japyrus', () => {
          it('should be HelloTamriel', () => {
@@ -16,10 +23,78 @@ mocha.run(() => {
             assert.deepEqual(japyrus.txt("abc").exec("abc", 0), { res: "abc", end: 3 });
          });
          it('parse Scriptname', ()=> {
-           //TODO: check if spaces empty
             let testScriptName = ScriptName;
             let result = japyrus.parseScriptname(testScriptName);
             assert.deepEqual(result, { scriptname: 'AAAMPChest', extends: 'extends' });
          });
+         it('parse arguments 1', ()=> {
+            let testArguments = "ObjectReference akActionRef";
+            let result = japyrus.parseArguments(testArguments);
+            console.log(result);
+            assert.deepEqual(result,  [ { type: 'ObjectReference', name: 'akActionRef' }]);
+         });
+         it('parse arguments 2', ()=> {
+            let testArguments = "ObjectReference akActionRef, ObjectReference akActionRef2";
+            let result = japyrus.parseArguments(testArguments);
+            console.log(result);
+            assert.deepEqual(result,  [ { type: 'ObjectReference', name: 'akActionRef' },  { type: 'ObjectReference', name: 'akActionRef2' } ]);
+         });
+         it('parse certain arguments 1', ()=> {
+            let testArguments = `233, "krab", "krab2"`;
+            let result = japyrus.parseCertainArguments(testArguments);
+            console.log(result);
+            assert.deepEqual(result,  [ '233', '"krab"', '"krab2"' ]);
+         });
+         it('parseCallOfFunction', ()=> {
+            let testCall = call;
+            let result = japyrus.parseCallOfFunction(testCall);
+            console.log(result);
+            assert.deepEqual(result,  { object: 'Debug',  method: 'MessageBox', args: [ '"Hello, World!"' ] });
+         });
+         it('parse Event', ()=> {
+            let testEvent = _Event;
+            let result = japyrus.parseEvent(testEvent);
+            console.log(result);
+            assert.deepEqual(result,  { blocktype: 'Event',  name: 'OnActivate',args: [ { type: 'ObjectReference', name: 'akActionRef' } ],body: [{ object: 'Debug',method: 'MessageBox',args: [ '"Hello, World!"', 'true' ] }]});
+         });
+         it('parse variable 1', ()=> {
+            let testVar = `int count = 432`;
+            let result = japyrus.parseVar(testVar);
+            assert.deepEqual(result,  { type: 'int', name: 'count', value: '432' });
+         });
+         it('parse variable 2', ()=> {
+            let testVar = `Float angleX`;
+            let result = japyrus.parseVar(testVar);
+            assert.deepEqual(result,  { type: 'float', name: 'angleX', value: undefined });
+         });
+         it('parse variable 3', ()=> {
+            let testVar = `Float angleX = 10.5`;
+            let result = japyrus.parseVar(testVar);
+            assert.deepEqual(result,  { type: 'float', name: 'angleX', value: '10.5' });
+         });
+         it('parse variable 4', ()=> {
+            let testVar = `sTRiNg _name2 = "clubnika"`;
+            let result = japyrus.parseVar(testVar);
+            assert.deepEqual(result,  { type: 'string', name: '_name2', value: '"clubnika"' });
+         });
+         it('parse wrong variable', ()=> {
+            let testVar = `!type! !name!`;
+            let result = japyrus.parseVar(testVar);
+            assert.deepEqual(result,  undefined);
+         });
+         it('parse wrong data', ()=> {
+            let result = japyrus.parseEvent(`Scriptname uncorrect 23 24`);
+            console.log(result);
+            assert.deepEqual(result, undefined);
+         });
+         it('get variable', ()=> {
+           japyrus.varlist.push({ type: 'int', name: 'count', value: '432' });
+           japyrus.varlist.push({ type: 'float', name: 'angleX', value: '10.5' });
+           japyrus.varlist.push({ type: 'string', name: '_name2', value: '"clubnika"' });
+           console.log(japyrus.varlist);
+           let result = japyrus.varlist.getVariable('count');
+           assert.deepEqual(result, { type: 'int', name: 'count', value: '432' });
+         });
+
      });
 });
